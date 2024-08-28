4aa8eb22e25e59bdac2ca7586135b24e2d1b2172
sumayaalabdallah <sumayaalabdallah@gmail.com>
2024-08-28 14:21:55 +0300

    Link the form API to the about us page

--- components.d.ts
+++ components.d.ts
@@ -7,40 +7,26 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-
-    BAccordion: typeof import('bootstrap-vue-next')['BAccordion']
-    BAccordionItem: typeof import('bootstrap-vue-next')['BAccordionItem']
-    BAlert: typeof import('bootstrap-vue-next')['BAlert']
-
     BButton: typeof import('bootstrap-vue-next')['BButton']
     BCard: typeof import('bootstrap-vue-next')['BCard']
     BCardBody: typeof import('bootstrap-vue-next')['BCardBody']
     BCardFooter: typeof import('bootstrap-vue-next')['BCardFooter']
     BCardHeader: typeof import('bootstrap-vue-next')['BCardHeader']
     BCardText: typeof import('bootstrap-vue-next')['BCardText']
-    BCardTitle: typeof import('bootstrap-vue-next')['BCardTitle']
     BCol: typeof import('bootstrap-vue-next')['BCol']
     BCollapse: typeof import('bootstrap-vue-next')['BCollapse']
     BContainer: typeof import('bootstrap-vue-next')['BContainer']
     BDropdown: typeof import('bootstrap-vue-next')['BDropdown']
-    BDropdownItem: typeof import('bootstrap-vue-next')['BDropdownItem']
     BForm: typeof import('bootstrap-vue-next')['BForm']
     BFormGroup: typeof import('bootstrap-vue-next')['BFormGroup']
     BFormInput: typeof import('bootstrap-vue-next')['BFormInput']
+    BFormSelect: typeof import('bootstrap-vue-next')['BFormSelect']
     BFormTextarea: typeof import('bootstrap-vue-next')['BFormTextarea']
-
-    BIconArrowRight: typeof import('bootstrap-vue-next')['BIconArrowRight']
-    BIconPatchCheck: typeof import('bootstrap-vue-next')['BIconPatchCheck']
     BListGroup: typeof import('bootstrap-vue-next')['BListGroup']
     BListGroupItem: typeof import('bootstrap-vue-next')['BListGroupItem']
-    BOffcanvas: typeof import('bootstrap-vue-next')['BOffcanvas']
-    BPopover: typeof import('bootstrap-vue-next')['BPopover']
-    BProgress: typeof import('bootstrap-vue-next')['BProgress']
-    BProgressBar: typeof import('bootstrap-vue-next')['BProgressBar']
     BRow: typeof import('bootstrap-vue-next')['BRow']
     BTab: typeof import('bootstrap-vue-next')['BTab']
     BTabs: typeof import('bootstrap-vue-next')['BTabs']
-
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
